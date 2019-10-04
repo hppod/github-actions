b13b2109ee03e4cbcc52ea5e45d2468665fee9f1
hppod <hppodgornik@gmail.com>
2019-10-03 23:26:37 -0300

    Set debug action output

--- .github/actions/debug-action/debug.ts
+++ .github/actions/debug-action/debug.ts
@@ -3,7 +3,9 @@ import * as core from '@actions/core'
 const run = async (): Promise<void> => {
     //Action code 🔥
     const creature = core.getInput('amazing-creature')
-    core.debug(`👋 Hello! You are an amazing ${creature}!! 🙌`)
+    const message = `👋 Hello! You are an amazing ${creature}!! 🙌`
+    core.debug(message)
+    core.setOutput('amazing-message', message)
 }
 
 run()
